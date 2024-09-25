c3915c8aa116a1d680c5e8fa876ebca676a1dea2
Gabsh3 <145166498+Gabsh3@users.noreply.github.com>
2024-09-24 21:15:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openWhatsApp() {
+    window.open('https://wa.me/5511956283341', '_blank');
+}
+
+function openInstagram() {
+    window.open('https://www.instagram.com/gabriell_h3', '_blank');
+}
